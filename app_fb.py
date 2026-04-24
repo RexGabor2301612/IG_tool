4afd11e5ffc9cf74e5e09840adab3e1db8cd744f
REX <2301612@lnu.edu.ph>
2026-04-24 23:47:29 +0800

    Removed Excel files

--- app_fb.py
+++ app_fb.py
@@ -1033,5 +1033,5 @@ def download_output():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", "5000"))
+    port = int(os.environ.get("PORT", "5001"))
     app.run(host="0.0.0.0", port=port, debug=True, use_reloader=False)
